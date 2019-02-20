231415833070315e43bcc8500d1ce25d4c4082ad
connormullett <connormullett@gmail.com>
2019-02-19 22:11:03 -0500

    building account object, connecting to user

--- pybank/models.py
+++ pybank/models.py
@@ -1,18 +1,37 @@
 
 import hashlib
 
+from enum import Enum
+
 
 class User(object):
     '''This class will handle creation, management,
     and other functionality of user accounts'''
 
+    user_id = 0
+
     def __init__(self, *args, **kwargs):
         '''requires pin, name can resort to default'''
         self._name = kwargs.get('name', User.default())
-        self._pin = self.encrypt(kwargs.get('pin', None))
+        self._user_id = User.generate_id()
 
+        if len(str(kwargs.get('pin', None))) == 4:
+            self._pin = self.encrypt(kwargs.get('pin', None))
+        else:
+            raise ValueError('Pin must be 4 digits long')
         if self.pin is None:
-            return 'No pin specified'
+            raise ValueError('No Pin specified')
+
+    def add_account(self, *args, **kwargs):
+        '''
+        creates an account instance and binds to the user
+        :params: pin, account_type: int (see AccountTypes Enumeration)
+        '''
+        pin = kwargs.get('pin', None)
+        account_type = kwargs.get('account_type', None)
+        if self.verify(pin) and account_type is not None:
+            return Account(account_type=account_type, user_id=self.user_id)
+        return None
 
     def encrypt(self, pin):
         '''encrypts the pin to SHA256 hash'''
@@ -23,8 +42,7 @@ class User(object):
         '''Verifies SHA256 Hash'''
         if self.encrypt(pin) == self.pin:
             return True
-        else:
-            return False
+        return False
 
     def login(self, *args, **kwargs):
         '''attempts to login user using verify method'''
@@ -57,6 +75,11 @@ class User(object):
     def default(cls):
         return 'default'
 
+    @classmethod
+    def generate_id(cls):
+        User.user_id += 1
+        return User.user_id
+
     # name property
     @property
     def name(self):
@@ -76,19 +99,51 @@ class User(object):
     def pin(self, value):
         self._pin = value
 
+    # id property, no setter
+    @property
+    def user_id(self):
+        return self._user_id
+
+
+class AccountTypes(Enum):
+    CHECKING = 1
+    SAVING = 2
+
 
 class Account(object):
+    '''this class will handle manipulations of
+       account instances'''
+
+    _account_id = 0
 
     def __init__(self, *args, **kwargs):
+        '''
+        params: user_id: int, account_type: int
+        '''
+        self._user_id = kwargs.get('user_id', None)
+        self._account_type = AccountTypes(kwargs.get('account_type', None))
+
+        self._account_id = Account.get_account_id()
+
+    @classmethod
+    def get_account_id(cls):
+        Account._account_id += 1
+        return Account._account_id
+
+    def deposit(self, *args, **kwargs):
+        pass
+
+    def withdraw(self, *args, **kwargs):
+        pass
+
+    def get_balance(self, *args, **kwargs):
         pass
 
 
-class Authenticator(object):
-    '''This class will handle authintcation
-    of information between user and client'''
-    pass
-
-
 class Exceptions:
+    '''
+    This class may be a throw away, used for any new exceptions
+    needed for this library
+    '''
     pass
 
